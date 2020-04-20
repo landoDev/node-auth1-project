c4d762836888308ed84ece5e9a823a56f5a5feb5
Landon Turner <lturner@teamworks.com>
2020-04-20 15:33:27 -0400

    adds get user endpoint

--- api/server.js
+++ api/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const userRouter = require('../users/user-router');
+const authRouter = require('../auth/auth-router');
 
 const server = express();
 
-server.use(helmet())
+server.use(helmet());
 server.use(express.json());
-server.use(cors())
+server.use(cors());
+
+server.use("/api/auth/users", authRouter);
+server.use("/api/users", userRouter);
 
 server.get("/api", (req, res) =>{
     res.send(`<h1>Auth Project API</h1>`)
--- users/user-router.js
+++ users/user-router.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const Users = require('./user-model')
-const router = express.Router;
+const router = express.Router();
 
-router.get('/users', (req, res) =>{
+router.get('/', (req, res) =>{
     Users.find()
     .then(users =>{
-        if(users){
-            res.status(200).json(users)
-        } else {
-            res.status(404).json({ message: 'No Data' })
-        }
+        res.status(200).json(users)
     })
     .catch(err =>{
         console.log(err);
