83bf9709ebfe5dc48229bc9784f00da5b0dadc05
Landon Turner <lturner@teamworks.com>
2020-04-20 15:23:23 -0400

    database running. adds user model and router

--- users/user-model.js
+++ users/user-model.js
@@ -1,3 +1,21 @@
+const db = require('../data/db-config')
+
 module.exports = {
-    
+    find,
+    findById,
+    add
+};
+
+function find(){
+    return db('users');
+};
+
+function findById(id){
+    return db('users').where({ id }).first();
+};
+
+async function add(user){
+    const [id] = await db('users').insert(user, 'id');
+
+    return findById(id);
 }
--- users/user-router.js
+++ users/user-router.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const Users = require('./user-model')
 const router = express.Router;
 
 router.get('/users', (req, res) =>{
-    
+    Users.find()
+    .then(users =>{
+        if(users){
+            res.status(200).json(users)
+        } else {
+            res.status(404).json({ message: 'No Data' })
+        }
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json({error: 'Could not find users'})
+    })
 })
 
 module.exports = router;
