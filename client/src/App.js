69cd5765dc7204c84e7137366d080e472379aaa4
Landon Turner <lturner@teamworks.com>
2020-04-20 19:56:38 -0400

    adds styles with css. styled components is bugged as of this commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Register from './components/Register';
+import Login from './components/Login';
 import { Route, NavLink } from 'react-router-dom';
 import { Button, Spinner } from 'reactstrap';
-import Login from './components/Login';
+import './App.css'
+import  { HeaderDiv }  from './styles/Styled.js';
+// v5 of styled looks like it has a react bug.
 
 
 
-function App() {
+const App = () => {
   const [usersMounted, setUsersMounted] = useState(false)
   const [loading, setLoading] = useState(false);
   const [users, setUsers] = useState();
@@ -29,27 +32,33 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>SHEILD HQ</h1>
+      </header>
+      <nav className="navigation">
         <NavLink to='/'>Home</NavLink>
         <NavLink to='/register'>Register</NavLink>
         <NavLink to='/login'>Login</NavLink>
-        <Route exact path='/'>
-          {!loading ? <Button color="warning" onClick={handleGetUsers}>Get Users</Button>
-          : <Spinner color="warning" />
-          }
-          {!usersMounted ? <div></div>
-          : users.map(user =>{
-            return <h2>{user.username}</h2>
-          })
-          }
-        </Route>
-        <Route exact path='/register'>
-          <Register />
-        </Route>
-        <Route path='/login'>
-          <Login />
-        </Route>
-        
-      </header>
+      </nav>
+      <Route exact path='/'>
+        {!loading ? 
+        <div className='grab-users'>
+           <Button color="danger" onClick={handleGetUsers}>Get Users</Button> 
+           </div>
+        : <div className='grab-users'>
+          <Spinner color="warning" />
+          </div>
+        }
+        {!usersMounted ? <div></div>
+        : users.map(user =>{
+          return <h2>{user.username}</h2>
+        })
+        }
+      </Route>
+      <Route exact path='/register'>
+        <Register />
+      </Route>
+      <Route path='/login'>
+        <Login />
+      </Route>
     </div>
   );
 }
